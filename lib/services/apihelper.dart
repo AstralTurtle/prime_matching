2584f2ea09fc8653335144d5589aec9088d498b5
AstralTurtle (Aaron James) <jamesacs329@gmail.com>
2024-02-09 13:21:22 -0500

    testLocal done

--- lib/services/apihelper.dart
+++ lib/services/apihelper.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
 
+
+import 'package:http/http.dart' as http;
 class APIhelper{
   static APIhelper _instance = APIhelper._internal();
 
@@ -8,5 +10,41 @@ class APIhelper{
 
   APIhelper._internal();
 
-  Future<Imag
+  Future<List<Image>> testLocal(){
+    Uri uri = Uri.http('localhost:5000', "/");
+    return http.get(uri).then((response) {
+      // response to json'
+      
+      var data = jsonDecode(response.body);
+      print(data["status"]);
+      // print(data["images"]);
+      // have to refactor api properly xddd
+      if (data["status"] == 200) {
+        
+        // get images array
+        var images = data["images"];
+        List<Image> imgList = [];
+        for (var image in images) {
+          var imgdata = Base64Decoder().convert(image);
+          imgList.add(Image.memory(imgdata));
+          
+        }
+
+        print("success");
+        return imgList;
+      } else {
+        throw Exception('Failed to get image');
+
+      }
+    
+    });
+  }
+
+  // Image<Convert>
+
+
+}
+
+void main(List<String> args) {
+  APIhelper().testLocal();
 }
